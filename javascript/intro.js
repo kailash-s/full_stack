9ed0f4e27b71b2703f33daff7f0b26fb06f9435a
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-02-19 20:20:12 +0000

    intro to javascript and readme updated

---
+++ javascript/intro.js
@@ -0,0 +1,8 @@
+// code is used to give instructions to computers like below for it to run:
+alert('hello');
+
+// on console, we can also do maths
+console.log(2+2);
+
+// we can also use it to change what is displayed on webpages
+document.body.innerHTML = 'hello';
