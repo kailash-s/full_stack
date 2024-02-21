80e91b023f7c78da6070cf350eef2e0c5c9d121d
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-02-21 22:27:58 +0000

    numbers finished with rounding and intro to strings with text, concatenation and type coercion

--- javascript/numbers.js
+++ javascript/numbers.js
@@ -7,3 +7,6 @@ console.log(2*2);
 // order of operations is followed in javascript as below:
 console.log(2+2*3);
 console.log(2+2*(2*2))
+
+// decimals are called floats and computers can have issues understanding them so we can use methods like below:
+console.log(Math.round(2.9756));
---
+++ javascript/strings.js
@@ -0,0 +1,8 @@
+// text in javascript is represented by strings with single quotes on each end
+console.log('some');
+
+// we can concatenate strings together like below:
+console.log('some'+'text');
+console.log('some'+'more'+'text');
+
+console.log('hello'+3);
