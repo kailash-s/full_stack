99e5ef06b5cf8b180f1f4b784b4a0886067d9c79
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-02-21 22:57:56 +0000

    strings finished with order of operations and template strings for interpolation

--- javascript/strings.js
+++ javascript/strings.js
@@ -1,8 +1,20 @@
-// text in javascript is represented by strings with single quotes on each end
+// text in javascript is represented by strings
 console.log('some');
 
 // we can concatenate strings together like below:
 console.log('some'+'text');
 console.log('some'+'more'+'text');
 
+// type coercion is when values are converted 
 console.log('hello'+3);
+
+// strings also follow order of operations
+console.log('£'+(5+2)); // give £7
+
+// there are 3 ways to make strings; single quotes (''), double quotes ("") and backticks (``) called tenmplate strings
+console.log('hello');
+console.log("hello");
+console.log(`hello`);
+
+// template strings allow interpolation as below:
+console.log(`£${2+2}`);
