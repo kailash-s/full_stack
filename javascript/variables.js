924c7fd7ca97cd5ae6fa1dad7677c0fa4da06d0a
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-02-24 11:02:49 +0000

    intro to variables with semi-colons and re-assigning

---
+++ javascript/variables.js
@@ -0,0 +1,22 @@
+// variable is like a container where we can store values
+let variable1 = 3;
+console.log(variable1);
+
+let calculation = 2+2;
+console.log(calculation);
+console.log(calculation+2);
+
+let result = calculation+2;
+console.log(result);
+
+// semi-colons let js know that it's the end of an instruction
+let message = 'hello';
+console.log(message);
+
+// we can re-assign a variable as below, no need for let again:
+variable1 = 5;
+console.log(variable1);
+
+// we can use variables within itself to imcrement the value
+variable1 = variable1+1;
+console.log(variable1);
