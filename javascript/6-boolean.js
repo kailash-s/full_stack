8b6106912d9e18b435bd38dec817bf607111aef1
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-02-25 10:53:21 +0000

    booleans cont'd with if-statements

--- javascript/6-boolean.js
+++ javascript/6-boolean.js
@@ -1,3 +1,26 @@
 // boolean value represents if something if true or false
 // the below code will show as false
 console.log(3>5);
+
+// we use comparison operators for booleans like <, >, <=, >=, ===, !==
+// in js, there is === and == but mostly === is used as
+console.log(5 == '5');
+// shows as true but
+console.log(5 === '5');
+// shows as false as that is correct
+
+// comparison operators also have lower priority than order of operations so
+console.log(3>5-3); 
+// shows as true
+
+// If-Statements lets us write multiple groups of code and decide which code to run
+if(true) {
+  console.log('hello');
+}
+
+// If-Statements also have a feature called else
+if (true) {
+  console.log('hello');
+} else {
+  console.log('else');
+}
