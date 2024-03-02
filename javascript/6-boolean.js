d9f652b2678d3511fd2051e3bb6a38aebef03d38
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-02 19:11:59 +0000

    booleans cont'd with else if and logical operators. rps project started

--- javascript/6-boolean.js
+++ javascript/6-boolean.js
@@ -1,26 +1,48 @@
-// boolean value represents if something if true or false
-// the below code will show as false
+// // boolean value represents if something if true or false
+// // the below code will show as false
 console.log(3>5);
 
-// we use comparison operators for booleans like <, >, <=, >=, ===, !==
-// in js, there is === and == but mostly === is used as
+// // we use comparison operators for booleans like <, >, <=, >=, ===, !==
+// // in js, there is === and == but mostly === is used as
 console.log(5 == '5');
-// shows as true but
+// // shows as true but
 console.log(5 === '5');
-// shows as false as that is correct
+// // shows as false as that is correct
 
-// comparison operators also have lower priority than order of operations so
+// // comparison operators also have lower priority than order of operations so
 console.log(3>5-3); 
-// shows as true
+// // shows as true
 
-// If-Statements lets us write multiple groups of code and decide which code to run
+// // If-Statements lets us write multiple groups of code and decide which code to run
 if(true) {
   console.log('hello');
 }
 
-// If-Statements also have a feature called else
-if (true) {
+// // If-Statements also have a feature called else
+if (false) {
   console.log('hello');
 } else {
   console.log('else');
 }
+
+// example
+let age = 30
+if (age>=16) {
+  console.log('You can drive');
+} else {
+  console.log('You cant drive');
+}
+
+// if-statements also have the else if feature
+let age = 15
+if (age>=16) {
+  console.log('You can drive');
+} else if(age=15) {
+  console.log('Almost');
+} else {
+  console.log('You cant drive');
+}
+
+// logical operators allow us to combine booleans
+console.log(true && false); // this will show as true
+console.log(true && false) // this will show as false
---
+++ project/javascript/06-rps.js
@@ -0,0 +1,10 @@
+const randonNumber = Math.random();
+
+function rockClicked() {
+const rock = document.getElementById('rock');
+console.log(rock);
+}
+
+if (randonNumber>=0 && randonNumber<1/3) {
+  console.log('rock');
+}
