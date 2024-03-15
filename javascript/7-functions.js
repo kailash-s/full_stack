545480a16c9a0406028a15175e5cdfc410de37a4
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-15 07:00:39 +0000

    functions exercises started

--- javascript/7-functions.js
+++ javascript/7-functions.js
@@ -19,3 +19,45 @@ function calculateTax(parameter1, taxPercent = 0.1) {
 
 calculateTax(2000, 0.2);
 calculateTax(5000);
+
+// EXERCISES
+// 7a, 7b
+function greet(name) {
+  console.log(`hello ${name}`);
+}
+greet('Harry');
+
+// 7c
+function greet(name) {
+  if (!name) {
+    console.log('Hi there');
+  } else {
+    console.log(`hello ${name}`);
+  }
+}
+greet();
+
+// 7d
+function convertToFahrenheit(celsius) {
+  return (celsius*9/5)+32;
+}
+console.log(convertToFahrenheit(25));
+
+// 7e
+function convertToCelcius(fahrenheit) {
+  return (fahrenheit-32)*5/9;
+}
+console.log(convertToCelcius(86));
+
+//7f
+function convertTemperature(degrees, unit) {
+  if (unit === 'C') {
+    const result = convertToFahrenheit(degrees);
+    return `${result}F`;
+  } else if (unit === 'F') {
+    const result = convertToCelcius(degrees);
+    return `${result}C`;
+  }
+}
+console.log(convertTemperature(25, 'C'));
+console.log(convertTemperature(86, 'F'));
