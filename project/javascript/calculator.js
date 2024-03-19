0df3b142f07bd06024833e8051db0a90ddda7293
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-19 07:32:07 +0000

    objects exercise finished with heads or tails game updated

--- project/javascript/calculator.js
+++ project/javascript/calculator.js
@@ -1,9 +1,9 @@
-let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
 
 // 7g
 function updateCalculation(value) {
   calculation += value;
   console.log(calculation);
-}
-
-// 8h adding localStorage
+  // 8h adding localStorage
+  localStorage.setItem('calculation', calculation);
+};
