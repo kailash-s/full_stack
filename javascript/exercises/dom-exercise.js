7ae0526810ee7c9edb09bb79366df50c394a548c
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-21 07:27:46 +0000

    dom projects cont'd with amazon shipping calculator started

--- javascript/exercises/dom-exercise.js
+++ javascript/exercises/dom-exercise.js
@@ -1,3 +1,4 @@
+// Youtube Subscribe Button
 function buttonClicked() {
 const buttonElement = document.querySelector('.js-subscribe-button');
 
@@ -6,4 +7,22 @@ if (buttonElement.innerText === 'Subscribe') {
 } else {
   buttonElement.innerHTML = 'Subscribe';
 }
+}
+
+// Amazon Shipping Calculator
+function calculateTotal() {
+  const inputElement = document.querySelector('.js-cost-input');
+  let cost = Number(inputElement.value);
+
+  if (cost<40) {
+    cost+=10;
+  }
+
+  document.querySelector('.js-total-cost').innerHTML = `$${cost}`;
+}
+
+function enterPressed(event) {
+  if (event.key === 'Enter') {
+    calculateTotal();
+  }
 }
