95f7a9c9b80171942324d6ab69a57c275a274fbf
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-26 06:37:48 +0000

    dom exercises finished and html, css and js together started with dom css

--- project/javascript/calculator.js
+++ project/javascript/calculator.js
@@ -4,6 +4,12 @@ let calculation = localStorage.getItem('calculation') || '';
 function updateCalculation(value) {
   calculation += value;
   console.log(calculation);
+  displayCalculation();
   // 8h adding localStorage
   localStorage.setItem('calculation', calculation);
 };
+
+// 9j dom used to display calculation
+function displayCalculation() {
+  document.querySelector('.js-calculate').innerHTML = calculation
+}
--- project/javascript/dom-projects.js
+++ project/javascript/dom-projects.js
@@ -4,8 +4,10 @@ const buttonElement = document.querySelector('.js-subscribe-button');
 
 if (buttonElement.innerText === 'Subscribe') {
   buttonElement.innerHTML = 'Subscribed';
+  buttonElement.classList.add('is-subscribed');
 } else {
   buttonElement.innerHTML = 'Subscribe';
+  buttonElement.classList.remove('is-subscribed');
 }
 }
 
