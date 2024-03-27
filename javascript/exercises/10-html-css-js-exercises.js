32130854690e19982f48ce5c7f66a3cb953a30d4
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-03-27 07:24:49 +0000

    hmtl, css & js exercises cont'd with classList.contains

--- javascript/exercises/10-html-css-js-exercises.js
+++ javascript/exercises/10-html-css-js-exercises.js
@@ -1,2 +1,22 @@
 // 10c
-console.log(document.querySelector('.js-button').classList.contains('js-button'));
+console.log(document.querySelector('.js-test-button').classList.contains('js-test-button'));
+
+// 10d, 10e, 10f
+function isToggled(selector) {
+  const button = document.querySelector(selector);
+
+  if (button.classList.contains('is-toggled')) {
+    button.classList.remove('is-toggled');
+  } else {
+    turnOffPreviousButton();
+    button.classList.add('is-toggled');
+  }
+}
+
+// 10g
+function turnOffPreviousButton () {
+  const previousButton = document.querySelector('.is-toggled');
+  if (previousButton) {
+    previousButton.classList.remove('is-toggled');
+  }
+}
